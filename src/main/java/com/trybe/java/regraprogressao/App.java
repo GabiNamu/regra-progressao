7247ae5719c01eb2867796e3d444f069ec96b23e
Gabriela Muniz <gabrielanamu@live.com>
2023-07-05 14:57:15 -0300

    feat: add req01 - 'Cadastrar atividades avaliativas'

--- src/main/java/com/trybe/java/regraprogressao/App.java
+++ src/main/java/com/trybe/java/regraprogressao/App.java
@@ -1,5 +1,7 @@
 package com.trybe.java.regraprogressao;
 
+import java.util.Scanner;
+
 /**
  * App.
  */
@@ -9,5 +11,20 @@ public class App {
    * Metodo main.
    */
   public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Digite a quantidade de atividades para cadastrar:");
+    String value = scanner.next();
+    int quantity = Integer.parseInt(value);
+    String[] examesNames = new String[quantity];
+    String[] examesWeight = new String[quantity];
+
+    for (int i = 0; i < quantity; i++) {
+      System.out.println("Digite o nome da atividade " + (i + 1) + ":");
+      examesNames[i] = scanner.next();
+      System.out.println("Digite o peso da atividade " + (i + 1) + ":");
+      examesWeight[i] = scanner.next();
+    }
+
+    scanner.close();
   }
 }
