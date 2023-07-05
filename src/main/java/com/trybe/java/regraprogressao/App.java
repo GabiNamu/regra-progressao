ce245005f77aca778d35189d88c78014f48aa2fa
Gabriela Muniz <gabrielanamu@live.com>
2023-07-05 15:25:11 -0300

    feat: add req02 - 'Inserir as notas obtidas'

--- src/main/java/com/trybe/java/regraprogressao/App.java
+++ src/main/java/com/trybe/java/regraprogressao/App.java
@@ -13,16 +13,21 @@ public class App {
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
     System.out.println("Digite a quantidade de atividades para cadastrar:");
-    String value = scanner.next();
+    String value = scanner.nextLine();
     int quantity = Integer.parseInt(value);
     String[] examesNames = new String[quantity];
     String[] examesWeight = new String[quantity];
+    String[] examesGrade = new String[quantity];
 
     for (int i = 0; i < quantity; i++) {
       System.out.println("Digite o nome da atividade " + (i + 1) + ":");
-      examesNames[i] = scanner.next();
+      String name = scanner.nextLine();
+      examesNames[i] = name;
       System.out.println("Digite o peso da atividade " + (i + 1) + ":");
-      examesWeight[i] = scanner.next();
+      String weight = scanner.nextLine();
+      examesWeight[i] = weight;
+      System.out.println("Digite a nota obtida para " + name + ":");
+      examesGrade[i] = scanner.nextLine();
     }
 
     scanner.close();
